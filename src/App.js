a318e5eefa26b1803c49a2af3802aadb247523a2
thyago608 <thyagoribeiro608@gmail.com>
2021-09-27 20:56:34 -0400

    Adde h2 element

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 
 function App() {
   return (
+    <div>
      <h1>Thyago Ribeiro</h1>
-  );
+      <h2>Operador de Transelevador</h2>
+    </div>
+    );
 }
 
 export default App;
